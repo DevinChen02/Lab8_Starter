6989082b564166aa2b6d623a8739bdc9cb0863d6
DevinChen02 <2090192426@qq.com>
2022-11-19 23:56:59 -0800

    finish lab8

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Lab 8 - Starter
+1) Option 1. The automated tests are fit within a Github actions that runs whenever code is pushed. Because in automation testing, the scripts are written to automatically test your new and existing software based on events like code getting pushed to the remote repository. 
+
+<br>
+
+2) No.
+
+<br>
+
+3) No, I would not. Because the "message" feature needs to test the process of writing and sending message to another user, but unit testing cannot test how the individual components interact with each other on a feature or application level.
+
+<br>
+
+4) Yes, I would. Because the "max message length" feature is a small scale feature without many moving parts, and we just want to test whether the feature could prevent the user from typing morn than 80 characters.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// **************** isPhoneNumber() ****************
+test('True Phone number 1', () => {
+    expect(functions.isPhoneNumber('(839)456-6247')).toBe(true);
+});
+
+test('True Phone number 2', () => {
+    expect(functions.isPhoneNumber('(839)-456-6247')).toBe(true);
+});
+
+test('False Phone number 1', () => {
+    expect(functions.isPhoneNumber('83-456-624')).toBe(false);
+});
+
+test('False Phone number 2', () => {
+    expect(functions.isPhoneNumber('83-454')).toBe(false);
+});
+
+// **************** isEmail() ****************
+test('True email 1', () => {
+    expect(functions.isEmail('cseucsd@ucsd.edu')).toBe(true);
+});
+
+test('True email 2', () => {
+    expect(functions.isEmail('cse110@ucsd.edu')).toBe(true);
+});
+
+test('False email 1', () => {
+    expect(functions.isEmail('123456ucsd')).toBe(false);
+});
+
+test('False email 2', () => {
+    expect(functions.isEmail('cse110ucsd.com')).toBe(false);
+});
+
+// **************** isStrongPassword() ****************
+test('True Strong Password 1', () => {
+    expect(functions.isStrongPassword('A1234_')).toBe(true);
+});
+
+test('True Strong Password 2', () => {
+    expect(functions.isStrongPassword('Abufsd79uwe')).toBe(true);
+});
+
+test('False Strong Password 1', () => {
+    expect(functions.isStrongPassword('1bufsd79uwe')).toBe(false);
+});
+
+test('False Strong Password 2', () => {
+    expect(functions.isStrongPassword('Abufsd7r65r566569uwe')).toBe(false);
+});
+
+// **************** isDate() ****************
+test('True Date 1', () => {
+    expect(functions.isDate('11/11/2022')).toBe(true);
+});
+
+test('True Date 2', () => {
+    expect(functions.isDate('1/11/2050')).toBe(true);
+});
+
+test('False Date 1', () => {
+    expect(functions.isDate('1/11/20590')).toBe(false);
+});
+
+test('False Date 2', () => {
+    expect(functions.isDate('april/11/2050')).toBe(false);
+});
+
+// **************** isHexColor() ****************
+test('True HexColr 1', () => {
+    expect(functions.isHexColor('#A83')).toBe(true);
+});
+
+test('True HexColr 2', () => {
+    expect(functions.isHexColor('#B83899')).toBe(true);
+});
+
+test('False HexColr 1', () => {
+    expect(functions.isHexColor('#A83874934227')).toBe(false);
+});
+
+test('False HexColr 2', () => {
+    expect(functions.isHexColor('cseucsd')).toBe(false);
+});
